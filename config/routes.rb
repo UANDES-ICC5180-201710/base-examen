bbc21ab20126600daa5c47d9467899f9dbe05a02
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-06-21 09:28:19 -0400

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :games
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root to: 'games#index'
 end
