5c51cb8fec4c01be752392b399a53dbb9847e24b
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-06-21 10:14:51 -0400

    Only logged in users can edit a game

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   resources :purchases
   devise_for :users
-  resources :games
   resources :users do
     get 'staff', to: 'users#staff', on: :member
   end
+
+  authenticate :user do
+    resources :games, only: [:new, :create, :edit, :update, :destroy]
+  end
+  resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   root to: 'games#index'
